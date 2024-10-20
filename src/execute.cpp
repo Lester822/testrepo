0c641e516ff3e37b7bd666b01d28a2184dc717c3
Michael S <mstang822@gmail.com>
2024-10-20 16:50:47 -0500

    test

--- src/execute.cpp
+++ src/execute.cpp
@@ -75,24 +75,7 @@ void cd_command(const vector<string>& args) {
     }
 }
 
-void sigchld_handler(int sig) {
-    while (waitpid(-1, NULL, WNOHANG) > 0);
-}
-
 int main() {
-    signal(SIGTTOU, SIG_IGN);
-    signal(SIGTTIN, SIG_IGN);
-
-    // Set up the SIGCHLD handler
-    struct sigaction sa;
-    sa.sa_handler = sigchld_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART; // To restart interrupted system calls
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-
     cout << "Welcome..." << endl;
     while (1) {
 
