f96eaa4476302bc6230587ee3f8f2b97e509c98f
Michael S <mstang822@gmail.com>
2024-10-19 20:23:27 -0500

    fixed merge issues (i hope)

--- src/execute.cpp
+++ src/execute.cpp
@@ -140,7 +140,7 @@ int main() {
                 } else if (commands[j][0] == "pwd") {
                     
                 } else if (commands[j][0] == "quit" || command[0] == "exit") {
-                    
+                    quexit_command();
                 } else if (commands[j][0] == "jobs") {
                     
                 }
@@ -152,12 +152,10 @@ int main() {
 
                 // Fork this process
                 // Run execute command to replace it with what we want, passing in arguments with it
-                
-            } else if (command[0] == "pwd") {
-                
-            } else if (command[0] == "quit" || command[0] == "exit") {
-                quexit_command();
-            } else if (command[0] == "jobs") {
+
+                if (last_elem != "|") {
+                    break;
+                }
                 
                 // Iterate through vector until we hit the end, a "|", or a ">"
                 // These indicate end of current command.
@@ -165,11 +163,9 @@ int main() {
                 // If we hit a "|" we need to pipe the output
                 // If we hit a ">" we need to put the output in a file
 
-                if (last_elem != "|") {
-                    break;
-                }
 
             }
+            
         }
 
 
